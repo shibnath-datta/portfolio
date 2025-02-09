73c088972ca9c0259d96542aa72bab9797969d60
shibnath-datta <132244714+shibnath-datta@users.noreply.github.com>
2025-02-09 22:24:39 +0600

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(
     server: {
       proxy: {
         '/api/': {
-          target: "http://localhost:5030"
+          target: "https://portfolio-front-taupe.vercel.app/"
         }
       }
     }
